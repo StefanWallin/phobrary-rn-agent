c1238e98bbb228f41721ad0d5f64201740d5ac7a
Stefan Wallin <stefan.w@festiz.com>
2019-05-16 08:47:00 +0200

    Suspend tests

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -3,13 +3,13 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have buttons on welcome screen', async () => {
+  xit('should have buttons on welcome screen', async () => {
     await expect(element(by.id('welcomeScreen'))).toBeVisible();
     await expect(element(by.id('getStartedButton'))).toBeVisible();
     await expect(element(by.id('signInButton'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
+  xit('should show hello screen after tap', async () => {
     await element(by.id('signInButton')).tap();
     await expect(element(by.id('signInScreen'))).toBeVisible();
     await expect(element(by.id('signInButton'))).toBeVisible();
